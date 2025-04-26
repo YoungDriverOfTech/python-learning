13c84cdc0bffedce334304bbc73025be1ab7fd27
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 20:46:34 +0900

    add comment

---
+++ 5_data_transform.py
@@ -0,0 +1,8 @@
+# Read user input
+user_weight = input("Input wight, number only")
+
+# transform data type
+print(float(user_weight))
+print(int(user_weight))
+print(str(user_weight))
+print(bool("true"))
