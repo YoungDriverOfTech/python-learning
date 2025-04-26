7d84064c6b111be4f7879cef606af163bfd82513
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 16:50:16 +0900

    add comment

--- 2_variables.py
+++ 2_variables.py
@@ -1,6 +1,8 @@
-# Use underline to connect word in a variable
-# Allowed character: word, number, underline.
-# Do not name a variable starts with number
-# All character lower case
+"""
+Use underline to connect word in a variable
+Allowed character: word, number, underline.
+Do not name a variable starts with number
+All character lower case
+"""
 new_variable = "hello world: variable"
 print(new_variable)
