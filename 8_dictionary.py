d2839159d9b5015aaa5cd031344671eb54dd4810
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 21:10:40 +0900

    dict

---
+++ 8_dictionary.py
@@ -0,0 +1,22 @@
+# Dic is like map in java
+# Create dictionary
+demo_dict = {
+    "aa": "AA",
+    "bb": "BB",
+    "cc": "CC"
+}
+
+# Printout dict
+print(demo_dict)
+print(len(demo_dict))
+
+# Get value
+print(demo_dict["aa"])
+
+# Delete element
+del demo_dict["aa"]
+print(demo_dict)
+
+# Update element
+demo_dict["bb"] = "BBB"
+print(demo_dict)
