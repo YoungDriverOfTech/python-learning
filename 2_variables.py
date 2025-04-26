a94253ddee168fd70d5448754cef45fcc701684c
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 16:41:14 +0900

    how to name variable

--- 2_variables.py
+++ 2_variables.py
@@ -1,4 +1,6 @@
 # Use underline to connect word in a variable
-# Allowed character: word, number, underline. Do not name a variable starts with number
+# Allowed character: word, number, underline.
+# Do not name a variable starts with number
+# All character lower case
 new_variable = "hello world: variable"
 print(new_variable)
