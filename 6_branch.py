7d19fa2961fae07e994613ed436680b1a3b3e931
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 21:03:39 +0900

    list

--- 6_branch.py
+++ 6_branch.py
@@ -1,7 +1,8 @@
+# and / or / not
 score = 80;
 if score >= 80:
     print("good")
-elif 60 <= score < 80: # 多个条件
+elif 60 <= score < 80: # 多个条件, 也可以使用 and
     print("passed")
 else:
     print("not passed")
---
+++ 7_list.py
@@ -0,0 +1,14 @@
+list = [1, 2, 3, 4, 5]
+list.remove(4)
+list.append(6)
+list[1] = 98
+
+max_val = max(list)
+min_val = min(list)
+sorted_list = sorted(list)
+list_len = len(list)
+
+print(max_val)
+print(min_val)
+print(sorted_list)
+print(list_len)
