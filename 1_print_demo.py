40befbb43429b73baab94ba2be5209bde493cce8
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 16:37:54 +0900

    Initial commit

---
+++ 1_print_demo.py
@@ -0,0 +1,11 @@
+# printout string
+print("hello world")
+
+# change newline
+print("hello \nworld")
+
+# string template
+print("""
+    hello
+    world
+""")
---
+++ 2_variables.py
@@ -0,0 +1,3 @@
+# Use underline to connect word in a variable
+new_variable = "hello world: variable"
+print(new_variable)
