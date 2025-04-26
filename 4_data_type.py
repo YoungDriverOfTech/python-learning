ff0fd7acf4db4d65855beca92c0c9cd34b58150e
liangxiaole <xiaoleliang045@gmail.com>
2025-04-26 17:02:12 +0900

    add comment

---
+++ 4_data_type.py
@@ -0,0 +1,20 @@
+# Get str len: len
+str1 = "hello world"
+print(len(str1))
+
+# Get first character: str1[0]
+print(str1[0])
+
+# Boolean, must use True & False, first letter is upper-case
+b1 = True
+b2 = False
+
+# Null
+b3 = None
+
+# get data type through type method
+print(type(str1))
+print(type(2))
+print(type(2.0))
+print(type(b1))
+print(type(b3))
